036eda0bda053f5d03f7a85e466a0e00631f8f96
arnas <gecas.arnas@gmail.com>
2017-02-10 18:29:40 +0200

    added flask port

--- server.py
+++ server.py
@@ -2,6 +2,10 @@
 from flask import Flask, jsonify, request
 import extractproducts
 import json
+import os
+
+
+port = int(os.environ.get("PORT", 5000))
 
 
 app = Flask(__name__)
@@ -77,6 +81,6 @@ def psu():
     return json.dumps([ob.__dict__ for ob in a.extract_all_products()])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
